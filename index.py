6553b1c5358a6664009831aad15b8bbe5354a483
balford <balfordm21@gmail.com>
2020-01-16 20:32:49 -0600

    While mejorado

--- index.py
+++ index.py
@@ -22,30 +22,28 @@ while True:
 print("Operaciones: Sumar    |   Restar   |   Multiplicar  |   Dividir")
 
 while True:
-    try:
-        op = (str(input("Ingresa la operacion a utilizar: ")))
-        op_=op.lower()
 
-        if op_ == "sumar":
-            print("Resultado = " + str(suma(val1, val2)))
-            break
+    op = (str(input("Ingresa la operacion a utilizar: ")))
+    op_=op.lower()
 
-        elif op_ == "restar":
-            print("Resultado = " + str(resta(val1, val2)))
-            break
+    if op_ == "sumar":
+        print("Resultado = " + str(suma(val1, val2)))
+        break
 
-        elif op_ == "multiplicar":
-            print("Resultado = " + str(multiplicar(val1, val2)))
-            break
+    elif op_ == "restar":
+        print("Resultado = " + str(resta(val1, val2)))
+        break
 
-        elif op_ == "dividir":
-            print("Resultado = " + str(dividir(val1, val2)))
-            break
+    elif op_ == "multiplicar":
+        print("Resultado = " + str(multiplicar(val1, val2)))
+        break
 
-    except ValueError:
+    elif op_ == "dividir":
+        print("Resultado = " + str(dividir(val1, val2)))
+        break
+
+    else:
         print("Has escrito mal la operacion. Intentelo Nuevamente")
 
 
 print("Operacion Ejecutada. Siga la ejecucion del Programa")
-
-#https://github.com/balfordmdiaz/Programa.FuncionesMath.git
